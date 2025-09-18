f461eb8de14de5fc8687eba375bf84110ff1f70f
Tiramonium <rodrigop2p@outlook.com>
2025-09-18 12:59:05 -0300

    Concentrated the package's exports under a single index.ts file

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+export { BaseClass } from './base.js';
+export type { WAConfigType } from './config.js';
+export { AudioMediaTypesEnum, ButtonPositionEnum, ButtonTypesEnum, ComponentTypesEnum, ConversationTypesEnum, CurrencyCodesEnum, DocumentMediaTypesEnum, HttpMethodsEnum, ImageMediaTypesEnum, InteractiveTypesEnum, LanguagesEnum, MessageTypesEnum, ParametersTypesEnum, ReferralSourceTypesEnum, StatusEnum, StickerMediaTypesEnum, SystemChangeTypesEnum, VideoMediaTypesEnum, WAConfigEnum, WARequiredConfigEnum, WebhookTypesEnum } from './enums.js';
+export { HttpsServerClass } from './httpsServer.js';
+export type { LoggerInterface } from './logger.js';
+export type { SemanticVersionLabelsType, SemanticVersionStringType } from './version.js';
+export { WhatsAppClass } from './WhatsApp.js';
+
+export { HttpsClientClass, HttpsClientResponseClass } from './httpsClient.js';
+export type { RequestDataType, RequestHeadersType, ResponseHeadersType, ResponseHeaderValueType, ResponseJSONBodyType, TimeoutErrorType } from './httpsClient.js';
+
+export { MessagesClass } from './messages.js';
+export type { AudioMediaType, AudioMessageRequestBodyType, ButtonComponentType, ButtonInteractiveType, ButtonParameterType, ContactMessageType, ContactsMessageRequestBodyType, CurrencyParametersType, DateTimeParametersType, DocumentMediaMessageType, DocumentMessageRequestBodyType, DocumentParametersType, GeneralMessageBodyType, HostedAudioMediaType, HostedDocumentMediaType, HostedImageMediaType, HostedStickerMediaType, ImageMediaMessageType, ImageMessageRequestBodyType, ImageParametersType, InteractiveMessageRequestBodyType, InteractiveMessageType, ListInteractiveType, ListSectionType, LocationMessageRequestBodyType, LocationMessageType, MessageActionType, MessageAddressesType, MessageButtonType, MessageComponentType, MessageContextType, MessageCurrencyType, MessageDateTimeType, MessageEmailType, MessageHeaderType, MessageLanguageType, MessageNameType, MessageOrgType, MessageParametersType, MessageProductType, MessageReplyButtonType, MessageRequestBodyType, MessageRowType, MessageSectionType, MessageSimpleTextType, MessagesResponseType, MessageTemplateRequestBodyType, MessageTemplateType, MessageURLType, MetaAudioMediaType, MetaDocumentMediaType, MetaHostedVideoMediaType, MetaImageMediaType, MetaStickerMediaType, MultiProductSectionType, ProductInteractiveType, QuickReplyButtonParametersType, ReactionMessageRequestBodyType, ReactionMessageType, SelfHostedVideoMediaType, StatusMessageType, StatusRequestBodyType, StickerMediaMessageType, StickerMessageRequestBodyType, TextMessageRequestBodyType, TextMessageType, TextParametersType, URLButtonParametersType, VideoMediaMessageType, VideoMessageRequestBodyType, VideoParametersType } from './messages.js';
+
+export { RequesterClass } from './requester.js';
+export type { GeneralHeaderInterface, GeneralRequestBodyType, RequesterResponseInterface } from './requester.js';
+
+export { TwoStepVerificationClass } from './twoStepVerification.js';
+export type { SetPinResponseType, TwoStepVerificationType } from './twoStepVerification.js';
+
+export { WebhooksClass } from './webhooks.js';
+export type { WebhookAudioType, WebhookButtonReplyType, WebhookButtonType, WebhookCallbackType, WebhookChangesType, WebhookContactType, WebhookContextType, WebhookConversationType, WebhookDocumentType, WebhookEntryType, WebhookErrorDataType, WebhookErrorType, WebhookIdentityType, WebhookImageType, WebhookInteractiveType, WebhookListReplyType, WebhookMessagesType, WebhookMetadataType, WebhookOrderType, WebhookOriginType, WebhookPricingType, WebhookProductItemsType, WebhookProfileType, WebhookReferralType, WebhookStatusesType, WebhookStickerType, WebhookSubscribeQueryType, WebhookSystemType, WebhookTextType, WebhookType, WebhookValueType, WebhookVideoType } from './webhooks.js';
